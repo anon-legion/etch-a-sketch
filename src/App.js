af5de7ed866a05eaaee90e483c61d6f96897f121
anon-legion <g.sync.mobile1@gmail.com>
2021-10-11 14:55:24 +0800

    update inputs and hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './App.css';
 import Grids from './components/grid.js';
 import randomRGB from './scripts/randomColor';
@@ -5,14 +6,35 @@ import randomRGB from './scripts/randomColor';
 const hoverFunction = (e) => {
   const [r, g, b] = randomRGB();
   e.target.style.background = `rgb(${r},${g},${b})`;
+  e.target.style.animation = 'hover-effect 700ms ease-in';
 }
+
 
 function App() {
 
+  const [gridSize, setGridSize] = useState(() => 16);
+  const [duration, setDuration] = useState(() => 3);
+  const [inputParam, setInputParam] = useState(() => ({
+    newSize: gridSize,
+    newDuration: duration
+  }));
+
+  const handleOnChange = (e) => {
+    setInputParam(prevState => {
+      return {...prevState, [e.target.name]: e.target.value}
+    });
+  }
+
   return (
     <div className="App">
+      <div className="App-settings">
+        <label htmlFor='inputSize'>Grid size:</label>
+        <input type='number' id='inputSize' name='newSize' value={inputParam.newSize} onChange={handleOnChange} min={2} max={256}/>
+        <label htmlFor='inputDuration'>Trail duration:</label>
+        <input type='number' id='inputDuration' name='newDuration' value={inputParam.newDuration} onChange={handleOnChange} min={-1} max={9}/>
+      </div>
       <div className="App-grid">
-        <Grids size={16} hoverEffect={hoverFunction} />
+        <Grids size={gridSize} hoverEffect={hoverFunction} trailDuration={duration * 1000}/>
       </div> 
     </div>
   );
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 
-const Grids = ({ size, hoverEffect }) => {
+const Grids = ({ size, hoverEffect, trailDuration }) => {
     let gridIndex = [];
     for (let i = 0; i < size ** 2; i++) {
         gridIndex.push(i);
     };
 
+    const handleMouseLeave = (e) => {
+        setTimeout(function delay() {
+            e.target.style.background = '#08060B';
+            e.target.style.animation = 'hover-effect 700ms ease-out'
+        }, trailDuration)
+    }
+
 
     return (
         <React.Fragment>
             {gridIndex.map(index => {
-                return <div key={index} onMouseOver={hoverEffect}></div>
+                return <div key={index} onMouseEnter={hoverEffect} onMouseLeave={handleMouseLeave}></div>
             })}
         </React.Fragment>
     );
